a8aa065a5d78b10a40d604dc3c4e8bc46c770183
cirill-junior <122447012+cirill-junior@users.noreply.github.com>
2023-05-19 23:30:29 +0300

    that git

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 # ToDo_app
-This project will be a ToDo app with the options to add and remove tasks form a basic list. 
+
+This project will be a ToDo app with the options to add and remove tasks form a basic list.
+
 - Add new tasks
 - Remove tasks
-- Added additional Google font 
-- Use of font awesome for icons 
+- Added additional Google font
+- Use of font awesome for icons
 - Create filter input (future itteration = FI)
 - Edit tasks (FI)
 - Add tasks to local storage (FI)
 - Prevention of duplicate (FI)
+- Add sum of tasks in filter and clear all (FI)
--- script.js
+++ script.js
@@ -2,33 +2,48 @@
 
 const taskForm = document.getElementById('task-form');
 const taskInput = document.getElementById('task-input');
-const tasklist = document.getElementById('task-list');
+const taskList = document.getElementById('task-list');
+const clearBtn = document.getElementById('clear');
+const taskFilter = document.getElementById('filter')
 
 
         //Validation of input
-function addTask(e) {
+function onAddItemSubmit(e) {
     e.preventDefault();
     const newTask = taskInput.value;
 
-        /* if (taskInput.value === '') {
-        alert(`Please add an new task`);
-        return; changing this to try getting the value of input*/
         if (newTask === ``) {
             alert(`Please add an new task`);
             return; 
         }
-        
+         
         //Create new task and add it to the list
+    createNewTask(newTask);
+        // Clear input field 
+    taskInput.value = '';
+
+    taskInput.focus();
+
+    }
+
+        //Create new task and add it to the list
+function createNewTask(newTask) {
     const li = document.createElement(`li`);
     li.appendChild(document.createTextNode(newTask));
 
         //creating the button for the task
-    const button = createButton(`remove-item btn-link text-red`)
+    const button = createButton(`remove-item btn-link text-red`);
     li.appendChild(button);
-    
-    tasklist.appendChild(li);
+        //add li to DOM
+    taskList.appendChild(li);
+    resetUI();
+}
 
-    function createButton(classes) {
+function addItemToDOM(task) {
+    
+}
+
+function createButton(classes) {
         const button = document.createElement(`button`);
         button.className = classes;
         const icon = createIcon(`fa-solid fa-check fa-sm`);
@@ -36,13 +51,60 @@ function addTask(e) {
         return button;
     }
         // creating the icon for checkmark
-    function createIcon(classes) {
+function createIcon(classes) {
         const icon = document.createElement(`i`);
         icon.className = classes;
         return icon;
     }
+
+function removeItem(e){
+    if (e.target.parentElement.classList.contains(`remove-item`)) {
+        if (confirm('Are you sure?')) {
+            e.target.parentElement.parentElement.remove();
+
+            resetUI();
+        }
+       
     }
+    
+}
+        // Clear all button
+function clearItems() {
+    taskList.innerHTML = '';
+    resetUI();
+}
 
-        // Event listener
-taskForm.addEventListener(`submit`, addTask);
+function filterItems(e) {
+    const tasks = taskList.querySelectorAll('li');
+    const text = e.target.value.toLowerCase();
+  
+    tasks.forEach((task) => {
+      const itemName = task.firstChild.textContent.toLowerCase();
+  
+      if (itemName.indexOf(text) != -1) {
+        task.style.display = 'flex';
+      } else {
+        task.style.display = 'none';
+      }
+    });
+  }
 
+function resetUI() {
+    const tasks = taskList.querySelectorAll('li');
+
+    if (tasks.length === 0) {
+       clearBtn.style.display = `none`; 
+       taskFilter.style.display = 'none';
+    } else {
+       clearBtn.style.display = 'block'; 
+       taskFilter.style.display = 'block'; 
+    }
+}
+
+        // Event listeners
+taskForm.addEventListener(`submit`, onAddItemSubmit);
+taskList.addEventListener(`click`, removeItem);
+clearBtn.addEventListener(`click`, clearItems);
+taskFilter.addEventListener(`input`, filterItems);
+
+resetUI();
